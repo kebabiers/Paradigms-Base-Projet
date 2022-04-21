d77a8216147a8a341bd1a35eba51e2eaaeb7297e
Thomas COLIN <thomas.colin@le-campus-numerique.fr>
2022-04-21 16:10:26 +0200

    filter et reduce

--- src/render-page.js
+++ src/render-page.js
@@ -123,10 +123,43 @@ const convert = (filter, transform) => (value) => {
 
 const map2 = (tab, fn, finalTab = [], cmpt = 0) => {
   if(finalTab.length >= tab.length) return finalTab;
-  finalTab.push(fn(tab[cmpt]))
-  cmpt ++
+  finalTab.push(fn(tab[cmpt]));
+  cmpt ++;
   return map2(tab, fn, finalTab, cmpt);
 }
 
-console.log(map2([1,2,3,4,5,12], x =>  x+1))
+function map3([first, ...rest], fn) {
+  if(rest.length === 1) {
+    return [fn(first), fn(rest)];
+  }
+  
+  return [
+    fn(first),
+    ...map3(rest)
+  ]
+}
+
+const filter2 = (tab, fn, finalTab = [], cmpt = 0) => {
+  if(cmpt >= tab.length) return finalTab;
+  if(fn(tab[cmpt])) finalTab.push(tab[cmpt]);
+  cmpt ++;
+  return filter2(tab, fn, finalTab, cmpt);
+}
+
+const reduce2 = (tab, fn, cmpt = 0, prev = tab[0]) => {
+  if(cmpt >= tab.length) return prev;
+  if (cmpt) {
+    prev = fn(prev, tab[cmpt]);
+  }
+  cmpt ++;
+
+  return reduce2(tab, fn, cmpt, prev);
+}
+
+
+
+// console.log(map2([1,2,3,4,5,12], x =>  x+1))
+// console.log(filter2([1,2,3,4,5,12], x => x > 2))
+// console.log(reduce2([4,2,3], (prev, curr) => prev * curr));
+
 module.exports = renderPage;
