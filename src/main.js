2c5feeb5f7344a9c49b14d12fcb1288c238e878e
benjamin <ferrandez.73000@yahoo.fr>
2022-04-22 16:11:41 +0200

    worked

--- src/main.js
+++ src/main.js
@@ -1,32 +1,50 @@
 const fetchData = require("./fetch-data");
 const { renderTable, renderGraph } = require("./render-page");
-const { fromEvent, throttleTime, scan, from, interval, timer, Observable, catchError, switchMap, of} = require("rxjs");
+const { fromEvent, combineLatest, throttleTime, scan, from, interval, timer, Observable, catchError, switchMap, of} = require("rxjs");
 const { fromFetch } = require("rxjs/fetch");
 
+const waiting = interval(1000);
 
-fromEvent(document, "DOMContentLoaded")
-  .pipe(
-    () => timer(0, 10000).subscribe(fetchData().then((data) => update(data)))
-  )
+
+// fromEvent(document, "DOMContentLoaded")
+//   .pipe(
+//     () => console.log('coucou'),
+//     () => waiting.subscribe(fetchData().then((data) => {
+//       console.log(data);
+//       update(data)
+//     })
+//   ))
 
 
 function update(data) {
+  console.log('coucou');
   renderGraph(data);
   renderTable(data);
 }
 
-// observable.subscribe({
-//   next(data) {
-//     console.log('pouet')
-//     renderTable(data);
-//     renderGraph(data); 
-//   }
-// });
+async function unpackdata(subscriber){
+  const data = await fetchData();
+  subscriber.next(data);
+}
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   update();
-//   setInterval(update, 10000); // Toutes les 10s, on récupère les dernières données et recrée la page.
-// });
+const observable = new Observable( subscriber => {
+    unpackdata(subscriber);
+});
+
+const observer = {
+  next: ([_,data]) => update(data)
+}
+
+observable.subscribe({
+  next(data) {
+    renderTable(data);
+    renderGraph(data); 
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  combineLatest(waiting,observable).subscribe(observer)
+});
 
 // async function update() {
 //   const data = await fetchData();
@@ -35,3 +53,5 @@ function update(data) {
 // async function fillData() {
 //   data = await fetchData();
 // }
+
+
